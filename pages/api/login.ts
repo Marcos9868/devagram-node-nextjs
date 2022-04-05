0e13626ade016d00e8200dea7d296abb0f00fdc3
Marcos9868 <marcos_m_ferreira@outlook.com>
2022-04-05 10:07:37 -0300

    Adiciona login simples de usuário

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -5,7 +5,12 @@ export default (
   res: NextApiResponse
 ) => {
   if (req.method === 'POST') {
+    const { login, senha } = req.body
 
+    if(login === 'admin@admin.com' && senha === 'admin@123') {
+      res.status(200).json({ mensagem: 'Usuário autenticado com sucesso' })
+    }
+    return res.status(400).json({ erro: 'Usuário e senha inválidos' })
   }
-  return res.status(405)
+  return res.status(405).json({ erro: 'Método informado não é válido' })
 }
