f68dfb59238ecab5e30391e41d67b876fecb701a
Marcos9868 <marcos_m_ferreira@outlook.com>
2022-04-05 13:37:32 -0300

    Adiciona um middleware de conexão ao mongoDB

---
+++ middlewares/dbConnection.ts
@@ -0,0 +1,32 @@
+import type { NextApiRequest, NextApiResponse, NextApiHandler } from 'next'
+import mongoose from 'mongoose'
+
+export const connectMongoDB = (handler: NextApiHandler) => {
+  async (req: NextApiRequest, res: NextApiResponse) => {
+    // Verifica se o banco está conectado
+    if(mongoose.connections[0].readyState) {
+      return handler(req, res)
+    }
+    // Não estando conectado
+    // Obter a variável de ambiente
+    const { DB_CONNECTION_STRING } = process.env
+
+    // Env vazio, avisa o programador
+    if(!DB_CONNECTION_STRING) {
+      return res.status(500).json({ erro: 'Env de configuração do db não informado' })
+    }
+
+    mongoose.connection.on('Connected', () => {
+      console.log('Banco de Dados Conectado')
+    })
+
+    mongoose.connection.on('Error', error  => {
+      console.log(`Ocorreu erro ao conectar no banco de dados: ${error}`)
+    })
+
+    await mongoose.connect(DB_CONNECTION_STRING)
+    
+    // Conectado, retorna aos endpoints da aplicação
+    return handler(req, res)
+  }
+}
