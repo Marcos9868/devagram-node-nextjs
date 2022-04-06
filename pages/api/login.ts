b23a6f18c3f7c425288237e1362f9c8259673104
Marcos9868 <marcos_m_ferreira@outlook.com>
2022-04-06 10:31:56 -0300

    Adiciona um type de resposta

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,19 +1,22 @@
 import type{ NextApiRequest, NextApiResponse } from 'next'
-import { connectMongoDB } from '../../middlewares/dbConnection'
+import connectMongoDB from '../../middlewares/dbConnection'
+import responseMessage from '../../@types/responseMessage'
 
 const loginEndpoint = (
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse<responseMessage>
 ) => {
   if (req.method === 'POST') {
     const { login, senha } = req.body
 
-    if(login === 'admin@admin.com' && senha === 'admin@123') {
-      res.status(200).json({ mensagem: 'Usuário autenticado com sucesso' })
+    if(login === 'marcos@admin.com' && senha === 'marcos123') {
+      return res.status(200).json({ message: 'Usuário autenticado com sucesso' })
     }
-    return res.status(400).json({ erro: 'Usuário e senha inválidos' })
+    
+    return res.status(400).json({ error: 'Usuário e senha inválidos' })
   }
-  return res.status(405).json({ erro: 'Método informado não é válido' })
+
+  return res.status(405).json({ error: 'Método informado não é válido' })
 }
 
 export default connectMongoDB(loginEndpoint)
